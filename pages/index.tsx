7582c2e6711da4f28e1848b44bc14262699c365c
cleggacus <cleggacus@gmail.com>
2022-06-10 19:58:56 +0100

    load animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import { useEffect } from 'react'
 import SectionGroup from '../components/core/sectiongroup'
 import Section1 from '../components/home/section1'
 import Section2 from '../components/home/section2'
